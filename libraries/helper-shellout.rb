bfc3fd42e540bb9b1ac44cd5d144e272e946ac0d
Brian Clark <brian.clark@steadyserv.com>
2015-08-08 15:02:38 -0400

    Use python `seobject` module via helper method to determine port/type mappings.

--- libraries/helper-shellout.rb
+++ libraries/helper-shellout.rb
@@ -24,6 +24,23 @@ class Chef
         Chef::Log.warn('SELinux is disabled / unreachable, skipping') if !return_val
         return return_val
       end
+
+      def port_defined(port, protocol)
+        cmd = <<-EOH.gsub(/^ {10}/, '')
+          python << END
+          import seobject, sys
+          pr = seobject.portRecords().get_all()
+          keys = filter(lambda x: x[0] <= #{port} <= x[1] and x[2] == '#{protocol}', pr.keys())
+          if(keys):
+            sys.stdout.write(pr[keys[0]][0])
+            exit(0)
+          else: exit(2)
+          END
+        EOH
+        result = shell_out!(cmd, returns: [0, 2])
+        defined = result.exitstatus == 0 ? true : false
+        return defined, result.stdout
+      end
     end
   end
 end
--- providers/port.rb
+++ providers/port.rb
@@ -7,34 +7,19 @@ end
 
 use_inline_resources
 
-def port_defined(port_b,port_t,proto)
-  "port_b=#{port_b};port_t=#{port_t};proto='#{proto}';context='#{context}'"+
-'import seobject
-pr=seobject.portRecords().get_all()
-keys=filter(lambda x: pr.keys());
-if(keys): exit(0)
-else: exit(2)'
-end
-
-def port_match_context(port_b,port_t,proto,context)
-  "port_b=#{port_b};port_t=#{port_t};proto='#{proto}';context='#{context}'"+
-'import seobject
-pr=seobject.portRecords().get_all()
-keys=filter(lambda x: pr.keys());
-if(keys):
-  val=pr[keys[0]][0]
-  if val==context: exit(0)
-  else: exit(1)
-else: exit(2)'
+def load_current_resource
+  @use_selinux = use_selinux
+  if @use_selinux
+    @defined, @context = port_defined(new_resource.port, new_resource.protocol)
+  end
 end
 
 # Create if doesn't exist, do not touch if port is already registered (even under different type)
 action :add do
   execute "selinux-port-#{new_resource.port}-add" do
     command "/usr/sbin/semanage port -a -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
-    guard_interpreter :python
-    not_if port_defined(new_resource.port,new_resource.port,new_resource.protocol)
-    only_if {use_selinux}
+    only_if { @use_selinux }
+    not_if { @defined }
   end
 end
 
@@ -42,30 +27,22 @@ end
 action :delete do
   execute "selinux-port-#{new_resource.port}-delete" do
     command "/usr/sbin/semanage port -d -p #{new_resource.protocol} #{new_resource.port}"
-    guard_interpreter :python
-    only_if port_defined(new_resource.port,new_resource.port,new_resource.protocol)
-    only_if {use_selinux}
+    only_if { @use_selinux && @defined }
   end
 end
 
 action :modify do
   execute "selinux-port-#{new_resource.port}-modify" do
     command "/usr/sbin/semanage port -m -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
-    only_if {use_selinux}
+    only_if { @use_selinux && @defined && @context != new_resource.secontext }
   end
 end
 
 action :addormodify do
+  option = @defined ? '-m' : '-a'
   execute "selinux-port-#{new_resource.port}-addormodify" do
-    command <<-EOT
-    if /usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null; then
-      /usr/sbin/semanage port -m -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}
-    else
-      /usr/sbin/semanage port -a -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}
-    fi
-    EOT
-    guard_interpreter :python
-    not_if port_defined(new_resource.port,new_resource.port,new_resource.protocol,new_resource.secontext)
-    only_if {use_selinux}
+    command "/usr/sbin/semanage port #{option} -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
+    only_if { @use_selinux }
+    not_if { @defined && @context == new_resource.secontext }
   end
 end
